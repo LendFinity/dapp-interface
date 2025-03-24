93ea00310201d41ead10db157a6182bc975d406e
Cláudio Barreira <claudio.barreira89@gmail.com>
2025-03-24 15:37:05 +0000

    feat: lukso

--- src/layouts/MobileMenu.tsx
+++ src/layouts/MobileMenu.tsx
@@ -13,6 +13,7 @@ import {
   Typography,
 } from '@mui/material';
 import { ReactNode, useEffect, useState } from 'react';
+import { PROD_ENV } from 'src/utils/marketsAndNetworksConfig';
 
 import { Link } from '../components/primitives/Link';
 import { moreNavigation } from '../ui-config/menu-items';
@@ -22,7 +23,7 @@ import { DrawerWrapper } from './components/DrawerWrapper';
 import { LanguagesList } from './components/LanguageSwitcher';
 import { MobileCloseButton } from './components/MobileCloseButton';
 import { NavItems } from './components/NavItems';
-// import { TestNetModeSwitcher } from './components/TestNetModeSwitcher';
+import { TestNetModeSwitcher } from './components/TestNetModeSwitcher';
 
 interface MobileMenuProps {
   open: boolean;
@@ -76,7 +77,7 @@ export const MobileMenu = ({ open, setOpen, headerHeight }: MobileMenuProps) => 
             <MenuItemsWrapper title={<Trans>Global settings</Trans>}>
               <List>
                 <DarkModeSwitcher />
-                {/* {PROD_ENV && <TestNetModeSwitcher />} */}
+                {!PROD_ENV && <TestNetModeSwitcher />}
                 {/* <LanguageListItem onClick={() => setIsLanguagesListOpen(true)} /> */}
               </List>
             </MenuItemsWrapper>
--- src/layouts/SettingsMenu.tsx
+++ src/layouts/SettingsMenu.tsx
@@ -2,11 +2,13 @@ import { CogIcon } from '@heroicons/react/solid';
 import { Trans } from '@lingui/macro';
 import { Button, Menu, MenuItem, SvgIcon, Typography } from '@mui/material';
 import React, { useState } from 'react';
+import { PROD_ENV } from 'src/utils/marketsAndNetworksConfig';
 
 // import { DEFAULT_LOCALE } from 'src/libs/LanguageProvider';
 // import { useRootStore } from 'src/store/root';
 // import { SETTINGS } from 'src/utils/mixPanelEvents';
 import { DarkModeSwitcher } from './components/DarkModeSwitcher';
+import { TestNetModeSwitcher } from './components/TestNetModeSwitcher';
 // import { LanguageListItem, LanguagesList } from './components/LanguageSwitcher';
 
 export const LANG_MAP = {
@@ -86,7 +88,7 @@ export function SettingsMenu() {
         </MenuItem>
 
         <DarkModeSwitcher component={MenuItem} />
-        {/* {PROD_ENV && <TestNetModeSwitcher />} */}
+        {!PROD_ENV && <TestNetModeSwitcher />}
         {/* <LanguageListItem onClick={handleLanguageClick} component={MenuItem} /> */}
       </Menu>
 
